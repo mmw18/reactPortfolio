46e96305fbdb61d54b153d4d9f77bc0e0670bb77
Megan Wright <meganmwright2018@gmail.com>
2024-01-07 15:55:02 -0600

    createBrowserRouter file structure and code

---
+++ src/routes/router.js
@@ -0,0 +1,24 @@
+import { createBrowserRouter } from 'react-router-dom';
+import AboutMe from '../components/aboutMe';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <AboutMe />,
+  },
+  {
+    path: "/projects",
+    element: <Projects />,
+  },
+  {
+    path: "/contactMe",
+    element: <Contact />,
+  },
+  {
+    path: "/resume",
+    element: <Resume />,
+  }
+
+]);
+
+export default router;
