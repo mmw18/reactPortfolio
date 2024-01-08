873822592f3ff0bd6474823cc8fd2e8a6e89e2bc
Megan Wright <meganmwright2018@gmail.com>
2024-01-08 13:09:07 -0600

    Restyling home page, changing font for header

--- src/components/home/home.jsx
+++ src/components/home/home.jsx
@@ -5,7 +5,7 @@ const Home = () => {
     return (
         <div className="home-container">
             <div className="welcome-message">
-                <h1>Where passion meets dedication, there unfolds the grand adventure of life, navigated by wisdom and fueled by an unyielding spirit.</h1>
+                <h1>Where passion meets dedication, there unfolds the grand adventure of life, navigated by wisdom and fueled by an unyielding spirit</h1>
             </div>
         </div>
     );
