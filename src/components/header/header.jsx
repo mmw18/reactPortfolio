a0231e17010a9b36987474a12986d11033c4c827
Megan Wright <meganmwright2018@gmail.com>
2024-02-03 15:49:06 -0600

    Change of homepage

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,16 +1,21 @@
 import React, { useState } from 'react';
 import './header.css';
-import { Link } from 'react-router-dom'; // Import Link from react-router-dom
+import { Link, useLocation } from 'react-router-dom'; // Updated import to include useLocation
 
 function Header() {
   const [isNavExpanded, setIsNavExpanded] = useState(false);
+  const location = useLocation(); // Using useLocation to get the current route
+  const isHomePage = location.pathname === '/'; // Checking if the current route is the homepage
+
   return (
     <header>
-      <div>
-        <Link to="/" className="overlay-title-link">
-          <h1 className="overlay-title">Megan Wright</h1>
-        </Link>
-      </div>
+      {!isHomePage && ( // Conditionally render the overlay title based on the route
+        <div>
+          <Link to="/" className="overlay-title-link">
+            <h1 className="overlay-title">Megan Wright</h1>
+          </Link>
+        </div>
+      )}
       <button className="hamburger" onClick={() => {
         setIsNavExpanded(!isNavExpanded);
         console.log("Nav Expanded:", !isNavExpanded);
@@ -19,10 +24,10 @@ function Header() {
       </button>
       <nav className={isNavExpanded ? "expanded" : ""}>
         <ul>
-          <li><a href="about-me">About Me</a></li>
-          <li><a href="projects">Portfolio</a></li>
-          <li><a href="contact">Contact Me</a></li>
-          <li><a href="resume">Resume</a></li>
+          <li><a href="/about-me">About Me</a></li> {/* Updated href to use '/' for consistency with routing */}
+          <li><a href="/projects">Portfolio</a></li>
+          <li><a href="/contact">Contact Me</a></li>
+          <li><a href="/resume">Resume</a></li>
         </ul>
       </nav>
     </header>
@@ -30,6 +35,7 @@ function Header() {
 }
 
 export default Header;
+
 
 
 
--- src/components/home/home.jsx
+++ src/components/home/home.jsx
@@ -1,14 +1,34 @@
 import React from 'react';
+import { TypeAnimation } from 'react-type-animation';
 import './home.css';
 
 const Home = () => {
     return (
         <div className="home-container">
+            <h1>Megan Wright</h1>
             <div className="welcome-message">
-                <h1>Where passion meets dedication, there unfolds the grand adventure of life, navigated by wisdom and fueled by an unyielding spirit</h1>
+                <TypeAnimation
+                    sequence={[
+                        'Website Developer', 1000, // Display time 2000ms
+                        'Passionate Learner', 1000,
+                        'Strategic Problem-Solver', 1000,
+                        'Spirited Adventurer', 1000,
+                        'Psychology Enthusiast', 1000,
+                        'Kind-Hearted Friend', 1000,
+                        () => {
+                            return '';
+                        }
+                    ]}
+                    wrapper="h1"
+                    cursor={true}
+                    repeat={Infinity}
+                    speed={185} // Typing speed in ms
+                    className="typing-animation"
+                />
             </div>
         </div>
     );
 };
 
 export default Home;
+
